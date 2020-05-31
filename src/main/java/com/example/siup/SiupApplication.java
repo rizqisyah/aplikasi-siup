13a8c594bd2dbb2eb89982fcf85113165c56b411
Rizqi <rizqi.putra@nostra.com>
2020-05-31 13:36:14 +0700

    first commit

---
+++ src/main/java/com/example/siup/SiupApplication.java
@@ -0,0 +1,13 @@
+package com.example.siup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/siup/SiupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.siup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
